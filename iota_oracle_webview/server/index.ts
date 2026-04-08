b1ed5dd466714bc850164f6b2a4c109167388318
sdellava <sdellava@gmail.com>
2026-04-08 13:54:15 +0200

    fix

--- iota_oracle_webview/server/index.ts
+++ iota_oracle_webview/server/index.ts
@@ -21,6 +21,12 @@ import { getOracleStatus } from "./services/oracleStatus.js";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("X-Frame-Options", "SAMEORIGIN");
+  res.setHeader("Content-Security-Policy", "frame-ancestors 'self';");
+  next();
+});
+
 app.use(cors());
 app.use(express.json({ limit: "2mb" }));
 
